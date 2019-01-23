adc607fbabad2ff4f7001cdc85728dca12ce5571
Matt Traudt <sirmatt@ksu.edu>
2019-01-22 20:16:19 -0500

    Add first attempt at args and logging

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,100 @@
 mod post;
 mod util;
 
-use post::file::File as PostFile;
+#[macro_use]
+extern crate structopt;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::path::Path;
 
 use tini::Ini;
+use structopt::StructOpt;
 
-use std::io::BufReader;
-use std::path::{Path,PathBuf};
-use util::fs;
+#[derive(StructOpt, Debug)]
+#[structopt(name="reb")]
+struct Args {
+    //#[structopt(short="v", long="verbose", parse(from_occurrences))]
+    ///// Say more things, repeat to make reb even louder
+    //verbose: u8,
+    #[structopt(subcommand)]
+    cmd: CommandArgs,
+}
+#[derive(StructOpt, Debug)]
+enum CommandArgs {
+    #[structopt(name="init")]
+    /// Initialize a directory for reb
+    Init {
+        #[structopt(short="f", long="force")]
+        /// Force overwriting existing config
+        force: bool,
+    },
+    #[structopt(name="build")]
+    /// Update the compiled blog
+    Build {
+        #[structopt(short="r", long="rebuild")]
+        /// Force a rebuild of all output files
+        rebuild: bool,
+    },
+}
 
-fn main() {
-    let conf = Ini::from_buffer(
-        vec!["[strings]",
-        "blog_title = My First Blog",
-        "blog_subtitle = Where I write about things and stuff",
-        "blog_author = John Doe",
-        "[paths]",
-        "post_dname = posts",
-        "build_dname = build",
-        ].join("\n"));
-    conf.to_file("conf.ini");
-    let s: String = conf.get("strings", "blog_title").unwrap();
-    println!("{}", s);
+fn init(args: Args) -> Result<(), String> {
+    trace!("Calling init with {:?}", args);
+    let path = Path::new("reb.ini");
+    let force = match &args.cmd {
+        CommandArgs::Init{force} => force,
+        _ => unreachable!(),
+    };
+    if path.exists() && !force {
+        let path = path.to_str().unwrap();
+        let s = format!("{} exists and refusing to overwrite it", path);
+        error!("{}", s);
+        return Err(s);
+    }
+    let path = path.to_str().unwrap();
+    info!("Writing default config to {}", path);
+    Ini::from_buffer("
+[strings]
+blog_title = My First BLog
+blog_subtitle = Where I write about things and stuff
+blog_author = John Doe
+
+[paths]
+post_dname = posts
+build_dname = build
+").to_file(path);
+    //if path.exists() && !args.cmd.force {
+    //    error!("{} exists and refusing to overwrite it", path.to_str().unwrap());
+    //}
+    //let config = get_default_config();
+    Ok(())
+}
+
+fn build(args: Args) -> Result<(), String> {
+    trace!("Calling build with {:?}", args);
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    env_logger::init();
+    let args = Args::from_args();
+    match args.cmd {
+        CommandArgs::Init{force} => init(args),
+        CommandArgs::Build{rebuild} => build(args),
+    }
+    //let conf = Ini::from_buffer(
+    //    vec!["[strings]",
+    //    "blog_title = My First Blog",
+    //    "blog_subtitle = Where I write about things and stuff",
+    //    "blog_author = John Doe",
+    //    "[paths]",
+    //    "post_dname = posts",
+    //    "build_dname = build",
+    //    ].join("\n"));
+    //conf.to_file("conf.ini");
+    //let s: String = conf.get("strings", "blog_title").unwrap();
+    //println!("{}", s);
 
     //let fnames = fs::recursive_find_files(&Path::new("./testdata"));
     //let fnames = fs::paths_with_extension(&fnames, ".reb");
